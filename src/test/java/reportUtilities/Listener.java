b9f26bac6167263809735a57e6e7bf7596cf1559
abidm-bit <abidmres@gmail.com>
2025-08-30 16:21:04 -0400

    Listener class config, return to this to fix the testFailure logging

---
+++ src/test/java/reportUtilities/Listener.java
@@ -0,0 +1,52 @@
+package reportUtilities;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class Listener implements ITestListener {
+
+    private static final ExtentReports extent = ExtentManager.createInstance("report.html");
+    private static final ThreadLocal<ExtentTest> methodTest = new ThreadLocal<>();
+    private static final Logger LOGGER = LogManager.getLogger (Listener.class);
+
+    private void logMessage(final String message){
+        LOGGER.info("\n");
+        LOGGER.info(message);
+        LOGGER.info("\n");
+    }
+
+    private ExtentTest getTest(ITestResult result) {
+        return methodTest.get();
+    }
+
+    @Override
+    public synchronized void onTestStart(ITestResult result) {
+        String methodName = result.getMethod().getMethodName();
+        ExtentTest test = extent.createTest(methodName);
+        methodTest.set(test);
+        logMessage ("Test Execution Started...." + result.getName ());
+        extent.flush();
+    }
+
+    @Override
+    public synchronized void onTestSuccess(ITestResult result) {
+        getTest(result).pass("Test passed");
+        logMessage ("Test Passed Successfully." + result.getName ());
+        extent.flush();
+    }
+
+    @Override
+    public synchronized void onTestFailure (ITestResult result) {
+        getTest(result)
+        .fail("Test failed due to: " + result.getThrowable().getMessage());
+        logMessage ("Test Failed!!!!" + result.getName ());
+        extent.flush();
+    }
+
+
+
+}
