3c13b258ea3324c57e2f3af188db3fba7ad52053
abidm-bit <abidmres@gmail.com>
2025-08-30 16:03:13 -0400

    Extent Report config class

---
+++ src/test/java/reportUtilities/ExtentManager.java
@@ -0,0 +1,22 @@
+package reportUtilities;
+
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+// A singleton class for generating a test report
+
+public class ExtentManager {
+
+// Private instance
+private static ExtentReports extent;
+
+// Create an instance
+public static ExtentReports createInstance(String file){
+    ExtentSparkReporter htmlReporter = new ExtentSparkReporter(file);
+    extent = new ExtentReports();
+    extent.attachReporter(htmlReporter);
+    return extent;
+}
+
+}
