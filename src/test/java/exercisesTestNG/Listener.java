cfe7858def0a48d681794df7f156d411d7f888e9
abidm-bit <abidmres@gmail.com>
2025-08-30 17:00:50 -0400

    moar configuration changes

--- src/test/java/exercisesTestNG/Listener.java
+++ src/test/java/exercisesTestNG/Listener.java
@@ -4,6 +4,7 @@ import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
@@ -14,9 +15,7 @@ public class Listener implements ITestListener {
     private static final Logger LOGGER = LogManager.getLogger (Listener.class);
 
     private void logMessage(final String message){
-        LOGGER.info("\n");
         LOGGER.info(message);
-        LOGGER.info("\n");
     }
 
     private ExtentTest getTest(ITestResult result) {
@@ -28,25 +27,26 @@ public class Listener implements ITestListener {
         String methodName = result.getMethod().getMethodName();
         ExtentTest test = extent.createTest(methodName);
         methodTest.set(test);
-        logMessage ("Test Execution Started...." + result.getName ());
-        extent.flush();
+        logMessage ("Test Execution Started for :" + result.getName ());
     }
 
     @Override
     public synchronized void onTestSuccess(ITestResult result) {
         getTest(result).pass("Test passed");
-        logMessage ("Test Passed Successfully." + result.getName ());
-        extent.flush();
+        logMessage ( result.getName () + " Passed Successfully " );
     }
 
     @Override
     public synchronized void onTestFailure (ITestResult result) {
         getTest(result)
         .fail("Test failed due to: " + result.getThrowable().getMessage());
-        logMessage ("Test Failed!!!!" + result.getName ());
-        extent.flush();
+        logMessage (result.getName () + " Test failed!!!" );
     }
 
-
-
+    @Override
+    public synchronized void onFinish(ITestContext context) {
+        logMessage ("Test execution completed for all tests!!" + context.getSuite ()
+                .getAllMethods ());
+        extent.flush();
+    }
 }
